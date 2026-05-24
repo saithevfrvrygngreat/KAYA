ac12eafc20be379f9f2dde2b707161c440837707
Dasetti Sai Kumar <148936657+saithevfrvrygngreat@users.noreply.github.com>
2026-05-24 21:00:23 +0530

    Configure dynamic SQLite copying and serverless logging/caching in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,31 @@
 <?php
 
+// 1. Setup SQLite database in writable /tmp directory
+$dbPath = '/tmp/database.sqlite';
+$sourceDb = __DIR__ . '/../database/database.sqlite';
+
+if (!file_exists($dbPath)) {
+    if (file_exists($sourceDb)) {
+        copy($sourceDb, $dbPath);
+    } else {
+        touch($dbPath);
+    }
+}
+
+// 2. Inject Vercel-compatible environment variables for serverless execution
+$vercelEnv = [
+    'DB_DATABASE' => $dbPath,
+    'LOG_CHANNEL' => 'stderr',
+    'CACHE_STORE' => 'array',
+    'SESSION_DRIVER' => 'cookie',
+    'VIEW_COMPILED_PATH' => '/tmp',
+];
+
+foreach ($vercelEnv as $key => $value) {
+    putenv("{$key}={$value}");
+    $_ENV[$key] = $value;
+    $_SERVER[$key] = $value;
+}
+
 // Forward Vercel requests to Laravel's public/index.php
 require __DIR__ . '/../public/index.php';
